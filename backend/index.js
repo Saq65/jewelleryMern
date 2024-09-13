5aae467de15cfa55f1915c7492c301259d020c98
saqlain shaikh <118292650+Saq65@users.noreply.github.com>
2024-09-13 15:46:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const authroute = require("../api/routes/authRoute");
+const authroute = require("../backend/routes/authRoute");
 const path = require('path');
 
 dotenv.config();
